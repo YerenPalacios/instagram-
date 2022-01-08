1b15bafd58ad910d4f7fac5b32d17afbc51aa340
Yeren Palacios <yerenagmt@gmail.com>
2022-01-08 18:17:18 -0500

    added login form and authentication token

--- src/components/haeader/header.js
+++ src/components/haeader/header.js
@@ -1,19 +1,115 @@
 import {default as ico} from '../icons'
 import './header.css'
+import { useState, useEffect, useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
+import gif from './ZKZg.gif'
+import api from '../../api.json'
+import { getUserSesion } from '../../helpers';
+
+//todo: finish this search
+
+function Search({users, loading}){
+    const usersList = users.map((e,i)=>(
+        <div key={i} className="user">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/1/1f/Woman_1.jpg" alt="" />
+            <div>
+                <p>TestUserName</p>
+                <p>User name</p>
+            </div>
+        </div>
+    ))
+    return(
+        <div className="search-list">
+            {!loading?
+            usersList
+            : <div className="loading">
+                <img src={gif} alt="" />
+            </div>
+            }
+        </div>
+    )
+}
+
+function useComponentVisible(initialIsVisible) {
+    const [isVisible, setIsComponentVisible] = useState(initialIsVisible);
+    const ref = useRef(null);
+
+    const handleClickOutside = (event) => {
+        if (ref.current && !ref.current.contains(event.target)) {
+            setIsComponentVisible(false);
+        }
+    };
+    const handleClickin = (event) => {
+        if (ref.current && ref.current.contains(event.target)) {
+            setIsComponentVisible(true);
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener('click', handleClickOutside, true);
+        document.addEventListener('click', handleClickin, true);
+        return () => {
+            document.removeEventListener('click', handleClickOutside, true);
+            document.removeEventListener('click', handleClickin, true);
+        };
+    }, []);
+
+    return { ref, isVisible, setIsComponentVisible };
+}
+
+function UserMenu({icon}){
+    const navigate = useNavigate()
+    const { ref, isVisible } = useComponentVisible(false);
+
+    const logout = () =>{
+        localStorage.removeItem('auth')
+        navigate('/login')
+    }
+
+    return(
+        <div className="user-menu" ref={ref}>
+            <div className="user-icon">
+                <img src={api.url+icon} alt="" />
+            </div>
+            {isVisible && (
+                <div className="menu">
+                    <p>{ico.profile} Perfil</p>
+                    <p>{ico.save} Guardado</p>
+                    <p>{ico.settings} Configuración</p>
+                    <p>{ico.change} Cambiar de cuenta</p>
+                    <p onClick={logout} >Salir</p>
+                </div>
+            )}
+        </div>
+    )
+}
 
 export default function Header(){
+    const [usersList, setUsersList] = useState([1])
+    const [showList, setShowList] = useState(false)
+    const {user} = getUserSesion()
+    const [listLoading, setListLoading] = useState(false)
+
+    const handleFocus = () => {
+        setShowList(!showList)
+    }
+
+
     return(
         <header>
             <img src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" alt="" className="logo" />
-            <input type="search" placeholder="Buscar"/>
+            <div className="search">
+                <input onBlur={handleFocus} onFocus={handleFocus} type="search" size="30" placeholder="Buscar"/>
+
+                {showList? <Search loading={listLoading} users={usersList}/> : null}
+            </div>
             <div className="icons">
                 <button>{ico.home}</button>
                 <button>{ico.share}</button>
                 <button>{ico.add}</button>
                 <button>{ico.find}</button>
                 <button>{ico.like_svg}</button>
-                <button>{ico.home}</button>
-
+                <UserMenu icon={user.image}/>
             </div>
         </header>
     )
--- src/components/storiesBar/storiesBar.js
+++ src/components/storiesBar/storiesBar.js
@@ -1,23 +1,35 @@
 import './storiesBar.css'
-import {useState, useEffect} from 'react'
+import {useState, useEffect, useRef} from 'react'
 import api from '../../api.json'
 import { Link } from 'react-router-dom'
+import testImg from '../../p.png'
+import {getToken} from '../../helpers'
 
 export default function StoriesBar(){
     const [profiles, setProfiles] = useState([]) 
 
     useEffect(() => {
-        fetch(api.url+'profile-stories/')
+        const controller = new AbortController()
+        const signal = controller.signal
+
+        fetch(api.url+'profile-stories/',{
+            signal:signal,
+            headers:{'Authorization':getToken()}
+        })
         .then(res => res.json())
         .then(data => setProfiles(data))
+        .catch((err)=>console.log(err.name))
+        
+        return () => controller.abort()
     }, [])
 
-    if (profiles.length) {
+
+    if (profiles.length != 0) {
         return(
             <div className="stories_bar">
                 {profiles.map((p,i)=>(
-                    <div className="profile">
-                        <img src={p.image} alt="" />
+                    <div key={i} className="profile">
+                        <img src={p.image? p.image : testImg} alt="" />
                         <p><Link to={"/"+p.name}>{p.name}</Link></p>
                     </div>
                 ))}
