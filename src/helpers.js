acd40d25694e0caf6723692a404a7358b3143764
Yeren Palacios <yeren.palacios@omnilatam.com>
2022-01-10 14:11:17 -0500

    update user and profile view

--- src/helpers.js
+++ src/helpers.js
@@ -1,3 +1,4 @@
+import api from './api.json'
 export const getToken = () => {
     let user = JSON.parse(localStorage.getItem('auth'))
     return 'Token '+user.token
@@ -9,4 +10,16 @@ export const getUserSesion = () => {
         return JSON.parse(authData)
     }
     window.location.href = '/login'    
+}
+
+export function UpdateUserSesion(user){
+    let authData = localStorage.getItem('auth')
+    if (authData){
+        authData = JSON.parse(authData)
+        user.image = user.image.replace(api.url,'')
+        authData.user = user
+        localStorage.setItem('auth', JSON.stringify(authData))
+        return true
+    }
+    return false
 }
