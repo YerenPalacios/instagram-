cd00eb1bf1378f1088c39159a6b0a56e5b9991ef
Yeren Palacios <yerenagmt@gmail.com>
2022-03-17 17:53:08 -0500

    added fetch hook

--- src/helpers.js
+++ src/helpers.js
@@ -1,25 +1,93 @@
 import api from './api.json'
+import {useState, useEffect} from 'react'
+import { useNavigate } from 'react-router-dom'
+
+
 export const getToken = () => {
     let user = JSON.parse(localStorage.getItem('auth'))
-    return 'Token '+user.token
+    return 'Token ' + user.token
 }
 
 export const getUserSesion = () => {
     let authData = localStorage.getItem('auth')
-    if (authData){
+    if (authData) {
         return JSON.parse(authData)
     }
-    window.location.href = '/login'    
+    window.location.href = '/login'
 }
 
-export function UpdateUserSesion(user){
+export function UpdateUserSesion(user) {
     let authData = localStorage.getItem('auth')
-    if (authData){
+    if (authData) {
         authData = JSON.parse(authData)
-        user.image = user.image.replace(api.url,'')
+        user.image = user.image.replace(api.url, '')
         authData.user = user
         localStorage.setItem('auth', JSON.stringify(authData))
         return true
     }
     return false
+}
+
+
+export function useFetch(url=null, options) {
+    const navigate = useNavigate()
+    const [data, setResponse] = useState(null);
+    const [error, setError] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+
+    const defaultOptions = {
+        method: 'GET',
+        headers:{
+            'Authorization':getToken()
+        },
+    }
+
+    if (!options) options = defaultOptions;
+
+    useEffect(() => {
+        const fetchData = async () => {
+            if (url){
+                setIsLoading(true);
+                try {
+                    const res = await fetch(api.url+url, options);
+                    if (!res.ok) throw(res)
+                    const json = await res.json();
+                    setResponse(json);
+                    setIsLoading(false);
+                } catch (error) { 
+                    setIsLoading(false)  
+                    setError(error);
+                }
+            }
+        };
+        fetchData();
+       
+    }, []); 
+
+    // try one day...
+    async function post(body){
+        options = { 
+            method: 'POST',
+            headers: {
+                'Authorization': getToken(),
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(body),
+        }
+        setIsLoading(true);
+        try {
+            options.method = 'POST'
+            const res = await fetch(api.url+url, options);
+            if (!res.ok) throw(res)
+            const json = await res.json();
+            setResponse(json);
+            setIsLoading(false);
+        } catch (error) { 
+            setIsLoading(false)  
+            setError(error);
+        }
+    }
+
+    if(error) if(error.status == 401) navigate('/login')
+    return {post, data, error, isLoading };
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -5,46 +5,19 @@ import Header from '../components/haeader/header'
 import {useNavigate} from 'react-router-dom'
 import StoriesBar from '../components/storiesBar/storiesBar'
 import './pages.css'
-import {api_, getToken} from '../helpers'
+import {useFetch} from '../helpers'
 import Error from '../components/errors/error'
 
 
-async function getData(path){
-    const res = await fetch(api.url+path,{
-        headers:{'Authorization':getToken()},
-    })
-    // if (!res.ok){
-    //     return false
-    // }
-    const data = await res.json()
-    return data
-    
-}
-
 export default function Home(){
-    const navigate = useNavigate()
     const [posts, setPosts] = useState([])
-    const [error, setError] = useState(false)
-    const url = api.url+'post/'
-    
-    const options = {
-        method: 'GET',
-        headers:{
-            'Authorization':getToken()
-        },
-    }
+
+    const res = useFetch('post/')
+    const error = res.error
     
     useEffect(()=>{
-        fetch(url, options)
-        .then(res=>{
-            if (!res.ok) setError(res)
-            return res.json()
-        })
-        .then(data => {
-            setPosts(data) 
-            setError(false)
-        })
-    },[])
+        if (res.data) setPosts(res.data)
+    },[res])
     
     var posts_list = posts.length? posts.map((post, i) => (
         <Post key={i} data={post}/>
@@ -52,11 +25,11 @@ export default function Home(){
 
     return(
         <div>
-            {error?<Error error={error}/>:null}
+            {error?<Error error={error}/>:null}            
             <Header/>
-            
             <div className="container">
                 <StoriesBar/>
+                {res.isLoading?<p>cargando...</p>:null}
                 {posts_list} 
             </div>
             
