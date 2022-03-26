bb80fb6786217007ba23dcf2c315142b987dd90b
Yeren Palacios <yeren.palacios@omnilatam.com>
2022-03-26 08:33:48 -0500

    added chatbox components

---
+++ src/components/chatContainer/chatContainer.js
@@ -0,0 +1,11 @@
+import ChatBox from '../chatBox/chatBox';
+import './chatContainer.scss';
+
+export default function ChatContainer(){
+    return(
+        <div className="chat-container">
+            <ChatBox/>
+        </div>
+    )
+    
+}
