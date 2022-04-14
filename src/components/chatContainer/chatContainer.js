755ce8f7b3494ddaff94ff7bf3f370615b23df88
Yeren Palacios <yerenagmt@gmail.com>
2022-04-14 18:58:57 -0500

    added send messages

--- src/components/chatContainer/chatContainer.js
+++ src/components/chatContainer/chatContainer.js
@@ -1,10 +1,35 @@
+import { useEffect, useState} from 'react';
 import ChatBox from '../chatBox/chatBox';
+import {useFetch} from '../../helpers'
 import './chatContainer.scss';
+import api from '../../api.json'
+import moment from 'moment'
 
 export default function ChatContainer(){
+    const res = useFetch('chatlist/')
+    const [users,setUsers] = useState([])
+    const [actualChat, setActualChat] = useState(null)
+
+    useEffect(() => {
+        if (res.data) setUsers(res.data)
+    }, [res])
+
     return(
         <div className="chat-container">
-            <ChatBox/>
+            <div className="chat">
+                <div style={{width:300}}>
+                    {users.map((i,k)=>(
+                        <div key={k} className="person" onClick={()=>setActualChat(i)}>
+                            <img src={api.url+i.image} alt="" />
+                            <div>
+                                <p>{i.name}</p>
+                                <p>{i.last_message.text} . {moment(i.last_message.created_at).from()}</p>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+                {actualChat&&<ChatBox user={actualChat}/>}
+            </div>
         </div>
     )
     
