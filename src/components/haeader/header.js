f6edf96687de14a6fc3afa59884599066bffef33
Yeren Palacios <yeren.palacios@omnilatam.com>
2022-12-17 14:33:37 -0500

    usin auth context

--- src/components/haeader/header.js
+++ src/components/haeader/header.js
@@ -1,4 +1,4 @@
-import {default as ico} from '../icons'
+import { default as ico } from '../icons'
 import './header.scss'
 import { useState, useEffect, useRef } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
@@ -7,11 +7,13 @@ import api from '../../api.json'
 import { getUserSesion } from '../../helpers';
 import NewPost from '../newPost/newPost';
 import testImg from '../../p.png'
+import { useContext } from 'react';
+import { AuthContext } from '../../context/datacontext';
 
 //todo: finish this search
 
-function Search({users, loading}){
-    const usersList = users.map((e,i)=>(
+function Search({ users, loading }) {
+    const usersList = users.map((e, i) => (
         <div key={i} className="user">
             <img src="https://upload.wikimedia.org/wikipedia/commons/1/1f/Woman_1.jpg" alt="" />
             <div>
@@ -20,13 +22,13 @@ function Search({users, loading}){
             </div>
         </div>
     ))
-    return(
+    return (
         <div className="search-list">
-            {!loading?
-            usersList
-            : <div className="loading">
-                <img src={gif} alt="" />
-            </div>
+            {!loading ?
+                usersList
+                : <div className="loading">
+                    <img src={gif} alt="" />
+                </div>
             }
         </div>
     )
@@ -59,24 +61,26 @@ function useComponentVisible(initialIsVisible) {
     return { ref, isVisible, setIsComponentVisible };
 }
 
-function UserMenu({icon, username}){
+function UserMenu() {
+    const { auth } = useContext(AuthContext)
     const navigate = useNavigate()
     const { ref, isVisible } = useComponentVisible(false);
 
-    const logout = () =>{
+    const logout = () => {
         localStorage.removeItem('auth')
         navigate('/login')
     }
 
-    return(
+    if (auth)
+    return (
         <div className="user-menu" ref={ref}>
             <div className="user-icon">
-                <img src={icon? api.url+icon: testImg} alt="" />
+                <img src={auth.user.image ? api.url + auth.user.image : testImg} alt="" />
             </div>
             {isVisible && (
                 <div className="menu">
-                    <Link to={"/"+username}>{ico.profile} Perfil</Link>
-                    <Link to={`${username}/saved`}>{ico.save} Guardado</Link>
+                    <Link to={"/" + auth.user.username}>{ico.profile} Perfil</Link>
+                    <Link to={`${auth.user.username}/saved`}>{ico.save} Guardado</Link>
                     <Link to={"/edit"}>{ico.settings} Configuración</Link>
                     <Link to="">{ico.change} Cambiar de cuenta</Link>
                     <p onClick={logout} >Salir</p>
@@ -84,39 +88,36 @@ function UserMenu({icon, username}){
             )}
         </div>
     )
+    else return null
 }
 
-export default function Header(){
+export default function Header() {
     const [usersList, setUsersList] = useState([1])
     const [showList, setShowList] = useState(false)
-    const {user} = getUserSesion()
     const [listLoading, setListLoading] = useState(false)
     const [showNewPostDiv, setShowNewPostDiv] = useState(false);
 
     const handleFocus = () => {
         setShowList(!showList)
-    }
+    }    
 
-
-    return(
+    return (
         <header>
             {/* <img src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" alt="" className="logo" /> */}
             <div className="search">
-                <input onBlur={handleFocus} onFocus={handleFocus} type="search" size="30" placeholder="Buscar"/>
+                <input onBlur={handleFocus} onFocus={handleFocus} type="search" size="30" placeholder="Buscar" />
 
-                {showList? <Search loading={listLoading} users={usersList}/> : null}
+                {showList && <Search loading={listLoading} users={usersList} />}
             </div>
             <div className="icons">
                 <Link to="/">{ico.home}</Link>
                 <Link to="/inbox">{ico.share}</Link>
-                <button onClick={()=>setShowNewPostDiv(true)}>{ico.add}</button>
+                <button onClick={() => setShowNewPostDiv(true)}>{ico.add}</button>
                 <button>{ico.find}</button>
                 <button>{ico.like_svg}</button>
-                <UserMenu username={user.username} icon={user.image}/>
+                <UserMenu />
             </div>
-            {showNewPostDiv?
-                <NewPost hide={setShowNewPostDiv}></NewPost>
-            :null}
+            {showNewPostDiv && <NewPost hide={setShowNewPostDiv}></NewPost>}
 
         </header>
     )
