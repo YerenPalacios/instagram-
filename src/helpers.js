99d9e1e42ec90f18e2abc6b11fe3f23f6b98eb12
Yeren Palacios <yeren.palacios@omnilatam.com>
2023-06-01 18:57:03 -0500

    small error fix

--- src/helpers.js
+++ src/helpers.js
@@ -1,5 +1,5 @@
 import api from './api.json'
-import { useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useContext } from 'react'
 import { ApiErrorContext, AuthContext } from './context/datacontext'
@@ -62,17 +62,16 @@ export const useFetch = (auto_errors = true) => {
             return navigate('/login')
 
         return fetch(
-            api.url + path, {signal: controller?.signal, ...options},
+            api.url + path, { signal: controller?.signal, ...options },
         ).then(res => {
-            if (res.status >= 500) {throw setError('Ha ocurrido un error')}
+            if (res.status >= 500) { throw setError('¡Ha ocurrido un error!') }
             if (res.status >= 400 && auto_errors)
                 res.json().then((data => {
                     throw setError(data[Object.keys(data)[0]])
                 }))
             else return res.json()
         }).catch(e => {
-            if(e.name !== 'AbortError')
-                throw setError('Ha ocurrido un error') 
+            throw setError('¡Ha ocurrido un error!')
         }).finally(() => setLoading(false))
     }
 
@@ -146,8 +145,8 @@ export const useFetch = (auto_errors = true) => {
 
 export function getUserImage(data) {
     // TODO: add user color field??
-    if (data.image) return data.image.includes('http')? data.image: api.url + data.image
-    const color = '#'+Math.floor(Math.random()*16777215).toString(16);
+    if (data.image) return data.image.includes('http') ? data.image : api.url + data.image
+    const color = '#' + Math.floor(Math.random() * 16777215).toString(16);
     const letter = data.username?.slice(0, 1).toUpperCase()
     const b = `<svg height="100" width="100" xmlns="http://www.w3.org/2000/svg">
     <rect width="100" height="100" style="fill:${color}" />
