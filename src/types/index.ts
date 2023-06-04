d604fbc2e18bdb0ee9e246272965f41af188a0d0
Yeren Palacios <yeren.palacios@omnilatam.com>
2023-06-04 09:38:22 -0500

    fixed comments and added ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -9,10 +9,38 @@ type User = {
     last_login: null | string,
     name: string,
     phone: string,
-    username: string
+    username: string,
+    color: string
 }
 
 type Auth = {
     token: string,
     user: User
 };
+
+type Image = {
+    image: string
+}
+
+type Post = {
+    comments_count: number,
+    created_at: string,
+    id: number,
+    images: Image[],
+    is_liked: boolean,
+    is_saved: boolean,
+    likes_count: number,
+    text: string,
+    user: User
+}
+
+type PostComment = {
+    created_at: string,
+    id: number,
+    parent: null,
+    post: number,
+    text: string,
+    user: User,
+    likes_count: number,
+    is_liked: boolean
+}
